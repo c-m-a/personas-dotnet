477c1427bb6e0a096fd0ccab6b62104ef8ec8998
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-09-19 01:03:49 -0500

    Add: Persona Controller logic

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace personas.Controllers {
+  [ApiController]
+  [Route("api/[controller]")]
+  public class PersonaController : ControllerBase
+  {
+    private readonly IPersonaService _personaService;
+
+    public PersonaController(IPersonaService personaService) {
+      _personaService = personaService;
+    }
+
+    [HttpGet("GetAll")]
+    public async Task<ActionResult<List<Persona>>> Get()
+    {
+      return Ok(await _personaService.GetAllPersonas());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Persona>> GetSingle(int id)
+    {
+      return Ok(await _personaService.GetPersonaById(id));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<List<Persona>>> AddPersona(Persona newPersona)
+    {
+      return Ok(await _personaService.AddPersona(newPersona));
+    }
+  }
+}
+
