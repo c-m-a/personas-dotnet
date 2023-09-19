9f797300f2394fe87958d08a6a37e7ee0d591457
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-09-19 01:04:21 -0500

    Add: Persona Model

---
+++ Models/Persona.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Persona
+{
+  public int Id { get; set; }
+  public string Nombres { get; set; } = "Anonimo";
+  public string Apellidos { get; set; } = "";
+  public string Cedula { get; set; } = "999999";
+}
+
