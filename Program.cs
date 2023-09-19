a32a7a8a78dc5ff4a2b6bdf7452942b8c8dfd8e6
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2023-09-19 16:21:33 -0500

    Add: Sql Server connection string context to main Program

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
-global using personas.Models;
-global using personas.Dtos.Persona;
-global using personas.Services.PersonaService;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using personas.Data;
+global using personas.Dtos.Persona;
+global using personas.Models;
+global using personas.Services.PersonaService;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
